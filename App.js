d3749c227efd1242739fc4c1545bd9c02d7ccd09
future <futurekgaphola@gmail.com>
2023-09-09 22:54:59 -0700

    first commit

--- App.js
+++ App.js
@@ -11,14 +11,14 @@ export default function App() {
 var [isHome,setHome]=useState(true);
 var [todos,seTodos]=useState(
 [
-{name:"Samuel",dob:'15 Jun 2024', msg: `that can be used as a pre-built solution! layground to view and fork react-native-shared-preferences`,id:'1'},
-{name:"Kenny",dob:'15 Nov 2023', msg: `Use this onlplayground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'2'},
-{name:"Karel Jubert",dob:'25 Dec 2023', msg: `Use this online react-native-shared-preferences playground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'3'},
-{name:"john",dob:'11 Dec 2023', msg: `Use this onlinelayground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'4'},
-{name:"Dave",dob:'11 April 2023', msg: `ences playground to view and forreferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'5'},
-{name:"Simon",dob:'23 Dec 2023', msg: `nces playground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'6'},
-{name:"Daniel",dob:'11 Dec 2023', msg: `Use this ew-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'7'},
-{name:"Roony Simons",dob:'13 Sept 2023', msg: `und to s example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'8'}
+{name:"Samuel Mankho",dob:'Wed Jun 15 2024', msg: `that can be used as a pre-built solution! layground to view and fork react-native-shared-preferences`,id:'1'},
+{name:"Kenny Okoje",dob:'Tue Jun 15 2024', msg: `Use this onlplayground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'2'},
+{name:"Karel Jones",dob:'Wed Jun 15 2024', msg: `Use this online react-native-shared-preferences playground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'3'},
+{name:"john",dob:'Mon Jun 15 2024', msg: `Use this onlinelayground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'4'},
+{name:"Dave",dob:'Thu Jun 15 2024', msg: `ences playground to view and forreferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'5'},
+{name:"Simon",dob:'Fri Jun 15 2024', msg: `nces playground to view and fork react-native-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'6'},
+{name:"Daniel",dob:'Wed Jun 15 2024', msg: `Use this ew-shared-preferences example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'7'},
+{name:"Roony Simons",dob:'Thu Jun 15 2024', msg: `und to s example apps and templates on CodeSandbox. Click any example below to run it instantly or find templates that can be used as a pre-built solution!`,id:'8'}
 ]
 );
 var [isvisible,showSnack]=useState(false);
--- components/ReminderForm.js
+++ components/ReminderForm.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-import { Image, Linking, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {Pressable, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Iconicon from "react-native-vector-icons/Ionicons";
 import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
-import { Button } from "react-native-paper";
 import * as ImagePicker from 'expo-image-picker';
 import Activityprogress from "./Activityprogress";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Platform } from "react-native";
 
 const ReminderForm = ({ setHome }) => {
+  const[date,setDate]=useState(new Date());
+  const[showPicker,setShowPicker]=useState(false);
   const [image, setImage] = useState(null);
   const[isSaving,setSave]=useState('save');
   const[isDisabled,setDisable]=useState(false);
+  const[dob,setdob]=useState('Day Mon DD YYYY');
   var uploading=()=>{
     
     try{
@@ -23,6 +27,23 @@ const ReminderForm = ({ setHome }) => {
       setDisable(false);
     }
   };
+  const toggleDatepicker=()=>{
+    setShowPicker(!showPicker);
+  }
+  const onChange=({type},selectedDate)=>{
+    if(type=="set"){
+      const currdate=selectedDate;
+      setDate(currdate);
+      if(Platform.OS==="android")
+      {
+        toggleDatepicker();
+        setdob(currdate.toDateString());
+      }
+    }else{
+      toggleDatepicker();
+    }
+    
+  }
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -52,12 +73,20 @@ const ReminderForm = ({ setHome }) => {
       </View>
       <View style={styles.containerinput}>
         <Iconicon name="calendar" size={24} color={"black"} />
-        <TextInput
-          style={{ marginLeft: 2 }}
-          placeholder="birth date"
-          keyboardType="default"
-          cursorColor={"black"}
-        />
+        {!showPicker && (
+          <Pressable
+          onPress={toggleDatepicker}>
+            <TextInput
+              style={{ marginLeft: 2 }}
+              placeholder="birth date"
+              keyboardType="default"
+              cursorColor={"black"}
+              editable={false}
+              onChange={setdob}
+              value={dob}
+            />
+          </Pressable>
+        )}
       </View>
 
       <View style={styles.containerinput}>
@@ -73,8 +102,6 @@ const ReminderForm = ({ setHome }) => {
       </View>
       <View style={styles.btncontainers}>
         <TouchableOpacity style={styles.modalbtn}
-        ><Text style={styles.modalText}><MaterialCommunityIcons name="clock" size={24} color="white" /> time</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.modalbtn}
           onPress={pickImage}
         ><Text style={styles.modalText}><Entypo name="folder-images" size={24} color="white" /> pick image</Text></TouchableOpacity>
         {image && <Image source={{ uri: image }} style={{ width: 40, height: 40, borderRadius: 180, alignSelf: "center" }} />}
@@ -87,6 +114,16 @@ const ReminderForm = ({ setHome }) => {
         <Text style={styles.BigText}>{isSaving==="save" ? <MaterialCommunityIcons name="content-save" size={20} color="white" /> :
         <Activityprogress/>}{isSaving}</Text>
       </TouchableOpacity>
+      {showPicker && (
+        <DateTimePicker
+          mode='date'
+          display='calendar'
+          value={date}
+          onChange={onChange}
+          timeZoneName={'Africa/Johannesburg'}
+          />
+      )}
+      
     </View>
   );
 };
@@ -125,7 +162,8 @@ const styles = StyleSheet.create({
   },
   card: {
     elevation: 3,
-    backgroundColor: "#fff",
+    borderRadius:5,
+    backgroundColor: "#FAF9F6",
     shadowOffset: { width: 1, height: 1 },
     shadowColor: "#333",
     shadowOpacity: 0.3,
@@ -133,6 +171,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 4,
     marginVertical: 6,
     margin: 15,
+    padding:5
   },
   modalbtn: {
     backgroundColor: 'black',
